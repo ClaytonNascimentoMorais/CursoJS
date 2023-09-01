424314eb7f1cc0528ff939eb2cd85be5a7db9f65
Clayton0800 <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2023-09-01 10:18:29 -0300

    atualização 01/09/2023

---
+++ aula11/index.js
@@ -0,0 +1,11 @@
+let num1 = 10.5564; // Number
+let num2 = 2.5; // Number
+
+ //console.log(num1.toString()); // mostra o numero como  uma String
+ //num1 = num1.toString(); // Transforma em string
+ //console.log(num1.toString(2)); // faz a transformação para binario pois entre aspas esta 2 se estiver 16 vira hexadecimal
+ //console.log(num1.toFixed(2)); // mostra o numero apenas com a qauntidade de casas decimais informada entra parenteses
+ //console.log(Number.isInteger(num1));// retorna true se o numero for inteiro e false caso contrario
+ //let temp = num1 * 'ola';
+ //console.log(Number.isNaN(temp))//informa se e possivel realizar a conta, caso seja possivel o retorno e false, caso nao seja possivel o retorno e NaN
+ 
---
+++ aula12/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.54578;
+//let num2 = Math.floor(num1);//arredonda o numero para baixo como numero inteiro.
+//let num2 = Math.ceil(num1); // arredonda para cima como numeiro inteiro.
+//let num2 = Math.round(num1);// arredona para o valor mais proximo, ou seja 0.49 arredonda para 0 e 0.5 arredonda para 1
+//console.log(Math.max(0,-1,5,2,20,-1));// Mostra qual o maior valor da sequencia.
+//console.log(Math.min(0,5,-1));// Mostra o menor valor da sequencia.
+//console.log(Math.random());// da um valor aleatorio entre 0 e 1
+//console.log(Math.random() * (20-10) + 10);// da um valor aleatorio entre 10 e vinte no caso
+//console.log(Math.round(Math.random()*(10-5)+5)) // essa função vai arredondar o valor aleatorio.
+//console.log(Math.PI);// valor de PI
+//console.log(Math.pow(2,3)); // potencia primeiro valor elevado ao segundo
