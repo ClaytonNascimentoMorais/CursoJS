3c28f547fc282822beec5e6643eba1d42b363772
Clayton0800 <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2023-09-14 10:13:25 -0300

    Atualização 13/09/23

---
+++ aula18/js/script.js
@@ -0,0 +1,31 @@
+/*
+Operadores de comparação
+> maior que
+>= maior igual
+<= menor igual
+< menor que
+== igualdade
+=== igualdade (valor e tipo)
+!= diferente
+!== diferente estrito (valor e tipo)
+*/
+/* 
+Operadores Lógicos
+&& -> AND -> false && true (retorna o valor falso)
+|| -> OR -> false || true (retorna o valor)
+!  _. NOT
+
+valores avaliados em falso FALSY VALUES
+* False (esse e o unico que seu valor e realmente falso)
+0
+' ', " ", ` ` (aspas crases e aspas duplas vazias)
+null \ undefined
+NaN
+*/
+
+//console.log('Luiz' && 'Maria'); // nesse caso ambos valores são verdadeiros logo irá retornar o ultimo valor (não há curto-circuito)
+
+//console.log('Luiz' && undefined && 'Maria'); // Nesse caso possuimos um curto circuito pois dois valores são verdadeiros e um falso, logo retorna o valor falso.
+
+//console.log(0 || false || null || 'Luiz' || true); // Neste caso retorna o primeiro valor verdaderio pois a função '||' precisa de apena um verdadeiro para retornar verdadeiro.
+
