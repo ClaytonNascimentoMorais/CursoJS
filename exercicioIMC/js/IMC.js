d858ab7ebb77592e09240d25cc09aa0e073811d6
Clayton0800 <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2023-09-19 10:17:45 -0300

    18/09/23

--- exercicioIMC/js/IMC.js
+++ exercicioIMC/js/IMC.js
@@ -2,11 +2,9 @@ function Calcular(){
     let peso = Number(document.getElementById('peso').value);
     let altura = Number(document.getElementById('altura').value);
     let imc = peso/ (altura*altura);
-    let result = document.getElementById('resultado')
-    if(peso == String || altura == NaN){
-        result.innerHTML += " invalido"
-    }
-    
+    let result = document.getElementById('resultado');
+    result.innerHTML = ` `;
+    result.innerHTML = ` Seu IMC`
     if(peso > 300 || altura > 2.4 || altura < 0.5){
         result.innerHTML += " Invalido"
     } else if(imc > 0 && imc < 18.5){
@@ -19,7 +17,8 @@ function Calcular(){
         result.innerHTML += `${imc.toFixed(2)}, Obsedidade grau 1`
     }else if(imc >= 34.9 && imc < 39.9){
         result.innerHTML += `${imc.toFixed(2)}, Obesidade grau 2`
-    }else if(imc >= 39.9){
+    }else {
         result.innerHTML += `${imc.toFixed(2)}, Obesidade grau 3`
     } 
+    
 }
