4f43938c3731a88977fc8d5d8c70ede1e16ecf9f
ClaytonNascimentoMorais <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2024-11-20 00:12:51 -0300

    Atualização
    Atualização das aulas 10,11 e 12 do curso da Udemy

---
+++ Aulas 10,11e12 Udemy/js/script.js
@@ -0,0 +1,21 @@
+/*let num1 = prompt('Digite um numero: ');
+let num2 = prompt('Digite outo numero: ');
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+ const resultado = num1 + num2;5
+alert(`O resultado foi ${resultado}`);*/
+
+let varA ='A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+let x;
+
+x = varA;
+varA = varB;
+varB = varC;
+varC = x;
+
+console.log(varA, varB, varC);
+
