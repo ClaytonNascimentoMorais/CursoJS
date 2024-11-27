cffec769a5c507093ea7747407c81930c223ee75
ClaytonNascimentoMorais <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2024-11-26 23:38:45 -0300

    atualização
    atualização de repositorio de estudo JavaScript

---
+++ aula10 (String funções)/js/script.js
@@ -0,0 +1,14 @@
+let umaString='Um texto';// caso eu use uma \ é um caractere de escape( logo é um espaçamento), para aparecer na mensagem a \ e necessario duas \\.
+console.log(umaString);
+
+console.log(umaString.concat('',' em um lindo dia')) //concatena a string da variavel com o texto colocado entre parenteses. 
+
+/*console.log(umaString.charAt(6));// pega o elemento na posição 6 da String
+console.log(umaString.indexOf('texto'));// para saber em qual posição começa o valor no caso 'texto', caso haja mais de um valor igual pode colocar para procurar apartir de uma valor 'indexof('texto',10)', nesse caso procuraria apartir do indice 10.
+console.log(umaString.lastIndexOf('t',4));// para procurar o valor de 'texto' de tras para frente apartir de um valor.
+console.log(umaString.replace('texto', 'carro'));// altera o valor da string para o valor seguinte.
+console.log(umaString.replace('t', '#'));// altera todas as letras 't' para '#'.
+console.log(umaString.length);//indica o tamanho da string.
+console.log(umaString.slice(3,8));// recorta apenas a parte indicada da string, caso coloque numero negativo o recorte será contado de tras para frente.
+console.log(umaString.toUpperCase);// deixa tudo em maiusculo.
+console.log(umaString.toLowerCase);//deixa tudo em minusculo.*/
