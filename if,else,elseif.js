71b7afc790aae21f2a593e556e86bf55fe86f676
ClaytonNascimentoMorais <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2024-12-19 23:04:36 -0300

    If, else, elseif

---
+++ if,else,elseif.js
@@ -0,0 +1,19 @@
+ /*const hora = 12
+if(hora <= 12){
+    console.log('Bom dia')
+}
+else if(hora <=18) {
+    console.log('Boa Tarde')
+}else{
+    console.log('Boa noite')
+}
+ // condição ternaria console.log(hora<12?"Bom dia":'Boa tarde')
+ */
+
+ const numero = 13;
+
+ if(numero >=0 && numero <=10){
+    console.log('Numeros primarios')
+ } else if(numero >=11 && numero <=20){
+    console.log('Segunda dezenas')
+ }else console.log('Numero alto')
