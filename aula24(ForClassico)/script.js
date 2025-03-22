9b4d120af92bd3a91c5b6a5db6f8acdee7aa2047
ClaytonNascimentoMorais <140182681+ClaytonNascimentoMorais@users.noreply.github.com>
2025-03-21 22:46:56 -0300

    Update script.js

--- aula24(ForClassico)/script.js
+++ aula24(ForClassico)/script.js
@@ -1,5 +1,9 @@
-const time = ['São paulo', 'Corinthians', 'Flamengo', 'Palmeiras'];
+/*const time = ['São paulo', 'Corinthians', 'Flamengo', 'Palmeiras'];
 
 for(let i =0; i< time.length; i++){
     console.log(`${i+1} ${time[i]}`);
+}*/
+
+for(let i =1; i <= 100; i+= 10){
+    console.log(`${i}° Olá`)
 }
